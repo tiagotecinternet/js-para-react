281f06355c402c926189de0b5b9d28ae13b7bc0d
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-09-22 10:57:56 -0300

    Revisando Arrow Functions

--- 02-arrow-functions.js
+++ 02-arrow-functions.js
@@ -39,3 +39,61 @@ function exercicio( valor1, valor2 ){
 
 let resultado = exercicio(70, 50);
 console.log(resultado);
+
+
+console.log("================================");
+
+/* Arrow Function (função "seta/flecha") 
+Possibilita diversas sintaxes, e até simplificação */
+const exemplo1 = () => { 
+    console.log('Uma arrow function qualquer...');
+};
+
+exemplo1();
+
+// Arrow Function com parâmetro
+//const ola = (nome) => {
+    
+// podemos omitir os parênteses no caso de 1 parâmetro
+/* const ola = nome => { 
+    console.log('Olá '+nome);
+}; */
+
+// Podemos omitir as {} no caso de uma única instrução
+const ola = nome => console.log('Olá '+nome);
+
+ola('Tiago');
+
+
+/* const metade = (valor) => {
+    return valor / 2;
+} */
+
+// Arrow Function com retorno IMPLÍCITO
+const metade = valor => valor / 2;
+
+console.log( metade(100) );
+
+console.log("=========");
+
+// Parâmetro com valor padrão (fator = 2)
+const multiplicaValores = (valor, fator = 2) => valor * fator;
+
+console.log( multiplicaValores(10, 3) );
+console.log( multiplicaValores(50) );
+
+
+
+/* Exercício
+- Monte uma arrow function que receba um nome de pessoa e a converta
+para letras maiúsculas.
+
+- Chame a função 3x (passando nomes diferentes) e exibindo no console
+os resultados */
+
+const converteMaiscula = nome => nome.toUpperCase();
+
+console.log( converteMaiscula('Tiago') );
+console.log( converteMaiscula('Neil') );
+console.log( converteMaiscula('alex') );
+
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Anotações
 
+## Principais tópicos de atenção e estudos
+
+- Declaração de variáveis e constantes (let e const)
+- Declaração e uso de funções (nomeadas e arrow functions), com e sem parâmetros e retorno de dados
+- Template Literals (Strings)
+
+
+## Sites úteis
 - https://www.w3schools.com/js/js_versions.asp
