b112defbb4dfa857d74828dcc9a47001d9c26ad9
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-09-27 08:48:30 -0300

    Update 05-classes.js

--- 05-classes.js
+++ 05-classes.js
@@ -13,6 +13,29 @@ class Pessoa {
     }
 }
 
+class Aluno extends Pessoa {
+    // Propriedades
+    matricula;
+    curso;
+
+    // Métodos
+    mostraDados(){
+        console.log(this.matricula);
+        console.log(this.curso);
+
+        // métodos da super classe
+        this.mostraNome();
+        this.mostraGenero();
+    }
+}
+
+
+
+
+
+
+
+
 /* Criando objetos/instâncias */
 let umaPessoa = new Pessoa;
 umaPessoa.nome = 'Tiago';
@@ -20,6 +43,21 @@ umaPessoa.genero = 'masculino';
 
 console.log(umaPessoa);
 
+let outraPessoa = new Pessoa;
+outraPessoa.nome = 'Mônica';
+
+console.log(outraPessoa);
+
+
+// Objeto herdando recursos
+let aluno = new Aluno;
+aluno.nome = 'Palloma';
+aluno.genero = 'feminino';
+aluno.matricula = '123abc';
+aluno.curso = 'JS para React';
+
+//console.log(aluno);
+aluno.mostraDados();
 
 
 
